e4e5ef4739f050df3e528c46fef61620be86c1a2
pando <spreadpando@gmail.com>
2019-05-18 13:48:55 -0600

    styles & threejs textsprites

--- assets/index.js
+++ assets/index.js
@@ -14,18 +14,22 @@ $(document).on('click', '.label', function () {
         scene.remove(scene.children[0]);
     }
     var nodes = [];
+    let subItems0 = ["about", "portfolio", "contact"];
+    let subItems1 = ["development", "design", "marketing"];
+    let subItems2 = ["repo", "repo", "repo", "repo", "repo"];
+    let subItems3 = ["social", "contact form"];
+
     let context = $(this).parent();
     $(".bullet", context).toggleClass('rot');
-
     if (context.hasClass('level0')) {
-        let subItems = ["about", "portfolio", "contact"];
-        nodes = subItems;
+
+        nodes = subItems0;
         if (context.hasClass('open')) {
             $('.level1').remove();
-            nodes = ['one'];
+            nodes = ['pando'];
         } else {
-            for (let i = 0; i < subItems.length; i++) {
-                setTimeout(function () { addItem(subItems[i], 1, i, 'nav') }, i * 100);
+            for (let i = 0; i < subItems0.length; i++) {
+                setTimeout(function () { addItem(subItems0[i], 1, i, 'nav') }, i * 100);
             }
         }
     }
@@ -33,78 +37,132 @@ $(document).on('click', '.label', function () {
         if (context.hasClass('index0')) {
             if (context.hasClass('open')) {
                 $('.level2').remove();
+                nodes = subItems0;
+
             } else {
-                let subItems = ["development", "design"];
-                nodes = subItems;
-                for (let i = 0; i < subItems.length; i++) {
-                    setTimeout(function () { addItem(subItems[i], 2, i, context) }, i * 100);
+                nodes = subItems1;
+                for (let i = 0; i < subItems1.length; i++) {
+                    setTimeout(function () { addItem(subItems1[i], 2, i, context) }, i * 100);
                 }
             }
         }
         if (context.hasClass('index1')) {
             if (context.hasClass('open')) {
                 $('.level3').remove();
+                nodes = subItems0;
+
             } else {
-                let subItems = ["repo", "repo", "repo"];
-                nodes = subItems;
-                for (let i = 0; i < subItems.length; i++) {
-                    setTimeout(function () { addItem(subItems[i], 3, i, context) }, i * 100);
+                nodes = subItems2;
+                for (let i = 0; i < subItems2.length; i++) {
+                    setTimeout(function () { addItem(subItems2[i], 3, i, context) }, i * 100);
                 }
             }
         }
         if (context.hasClass('index2')) {
             if (context.hasClass('open')) {
                 $('.level4').remove();
+                nodes = subItems0;
             } else {
-                let subItems = ["social", "contact form"]
-                nodes = subItems;
-                for (let i = 0; i < subItems.length; i++) {
-                    setTimeout(function () { addItem(subItems[i], 4, i, context) }, i * 100);
+                nodes = subItems3;
+                for (let i = 0; i < subItems3.length; i++) {
+                    setTimeout(function () { addItem(subItems3[i], 4, i, context) }, i * 100);
                 }
             }
         }
-
     }
-
     context.toggleClass('open');
-
-    var constellation = new THREE.Geometry();
-    for (let i = 0; i < nodes.length; i++) {
-        var cube = new THREE.Vector3();
-        if (i > 0) {
-            cube.x = Math.random() * 10;
-            cube.y = Math.random() * 10;
-            cube.z = Math.random() * 10;
-        }
-        constellation.vertices.push(cube);
-        scene.add(cube);
-    }
-    var lineMaterial = new THREE.LineBasicMaterial({
-        color: 0x000000,
-        depthTest: true,
-        opacity: 1,
-        transparent: true
-    });
-    var line = new THREE.LineLoop(constellation, lineMaterial);
-    scene.add(line);
-
+    render(nodes);
 })
-
-
-
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 5000);
 var renderer = new THREE.WebGLRenderer({ alpha: true });
 renderer.setSize(document.getElementById('wrap-body').offsetWidth, document.getElementById('wrap-body').offsetHeight);
 $('#wrap-body').append(renderer.domElement);
+camera.position.z = 5;
 
-camera.position.z = 25;
+function render(nodes) {
+    var group = new THREE.Group;
+    var banners = new THREE.Group;
+    var starsGeometry = new THREE.Geometry();
+    for (var i = 0; i < nodes.length; i++) {
+        var starGeometry = new THREE.Geometry();
+        var canvas = document.createElement("canvas");
+        var size = 4096;
+        canvas.width = size;
+        canvas.height = size;
+        var context1 = canvas.getContext('2d');
+        context1.fillStyle = "#ffffff";
+        context1.textAlign = "left";
+        context1.font = "normal 100px Work Sans, monospace";
+        context1.fillText(nodes[i], size / 2.1, size / 2.1);
+        var texture1 = new THREE.Texture(canvas);
+        texture1.needsUpdate = true;
+        var star = new THREE.Vector3();
+        star.x = THREE.Math.randFloatSpread(4);
+        star.y = THREE.Math.randFloatSpread(4);
+        starGeometry.vertices.push(star);
+        starsGeometry.vertices.push(star);
+        var textMaterial = new THREE.PointsMaterial({
+            size: 10,
+            map: texture1,
+            depthTest: false,
+            transparent: true
+        });
+        var pointMaterial = new THREE.PointsMaterial({
+            size: 0.1,
+            color: 0xffffff,
+            depthTest: true,
+            transparent: true,
+            //colorsneedsUpdate: true
+        });
+        var bulletPoint = new THREE.Points(starGeometry, pointMaterial);
+        var banner = new THREE.Points(starGeometry, textMaterial);
 
-var animate = function () {
-    requestAnimationFrame(animate);
+        switch (i) {
+            case i:
+                bulletPoint.name = nodes[i];
+                break;
+        }
+        banner.add(bulletPoint)
+        banners.add(banner);
+    }
+    group.add(banners);
+    var lineMaterial = new THREE.LineDashedMaterial({
+        vertexColors: false,
+        color: 0xffffff,
+        dashSize: .1,
+        gapSize: .2,
+        scale: 1,
+    });
 
-    renderer.render(scene, camera);
-};
+    var line = new THREE.LineLoop(starsGeometry, lineMaterial);
+    line.computeLineDistances();
+    group.add(line);
+    scene.add(group);
 
-animate();
+
+
+
+
+
+
+    var animate = function () {
+        requestAnimationFrame(animate);
+        group.rotation.z += Math.random() * 0.005;
+
+        renderer.render(scene, camera);
+    };
+    animate();
+}
+window.addEventListener('resize', onWindowResize, false);
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+
+render(['pando'])
